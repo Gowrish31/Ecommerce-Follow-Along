7bf81bb494f32e92854cb8573bee3ac815e89313
unknown <Gowrishankar.s@btech.christuniversity.in>
2025-01-06 16:12:20 +0530

    11

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -57,4 +57,15 @@ const createProductController = async (req, res) => {
   }
 };
 
-module.exports = { createProductController };
+const getProductDataController = async (req, res) => {
+  try {
+    const data = await ProductModel.find();
+    return res
+      .status(200)
+      .send({ data, message: 'Data Fetched Successfully', success: true });
+  } catch (er) {
+    // 500
+    return res.status(500).send({ message: er.message, success: false });
+  }
+};
+module.exports = { createProductController, getProductDataController };

74bccb40c94d1863ee406d118c46af7381d178e0
unknown <Gowrishankar.s@btech.christuniversity.in>
2025-02-07 15:14:34 +0530

    m27

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -1,49 +1,82 @@
-const { default: mongoose } = require("mongoose")
-const orderModel = require("../models/order.model")
-
-async function CreateOrder(req,res){
-    const UserId=req.UserId
-    const {items,address,totalAmount}=req.body
-    try{
-        if(!mongoose.Types.ObjectId.isValid(UserId)){
-            return res.status(400).send({message:"Invalid User Id",success:false})
-        }
-        const checkUser=await UserModel.findOne({_id:UserId})
-        if (!checkUser){
-            return res.status(400).send({message:'user not present please signup',success:false})
-        }
-        if (!items){
-            return res.status(400).send({message:"Items not present",success:false})
-        }
-        const order = await orderModel.create({
-            user:UserId,
-            orderItems:items,
-            shippingAddress:address,
-            totalAmount:totalAmount
-        })
-        return res.status(201).send({message:" Data successfully fetched",success:true,order})
-    }catch(er){
-        return res.status(500).send({message:er.message,success:false})
+const { mongoose } = require('mongoose');
+const OrderModel = require('../models/Order.model.js');
+const CartModel = require('../models/cart.model.js');
+const UserModel = require('../models/user.model.js');
+async function CreateOrderController(req, res) {
+  const userId = req.UserId;
+  const { Items, address, totalAmount } = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res
+        .status(400)
+        .send({ message: 'Invalid User Id', success: false });
     }
+    const checkUser = await UserModel.findOne({ _id: userId });
+    if (!checkUser) {
+      return res
+        .status(400)
+        .send({ message: 'Users not present pls Signup', success: false });
+    }
+
+    if (!Items) {
+      return res
+        .status(400)
+        .send({ message: 'Items not present', success: false });
+    }
+    const order = Items.map(async (ele) => {
+      return await OrderModel.create({
+        user: userId,
+        orderItems: ele.productId._id,
+        shippingAddress: address,
+        totalAmount: totalAmount,
+      });
+    });
+    await Promise.all(order);
+
+    const ItemsMapped = Items.map(async (eachItem) => {
+      return await CartModel.findByIdAndDelete({ _id: eachItem._id });
+    });
+    const checkDeletedItems = await Promise.all(ItemsMapped);
+
+    return res.status(201).send({
+      message: 'Data Successfully fetched',
+      success: true,
+      checkDeletedItems,
+    });
+  } catch (er) {
+    return res.status(500).send({ message: er.message, success: false });
+  }
 }
 
-async function getUserOrders(req,res){
-const UserId=req.UserId
-try{
-if (mongoose.Types.ObjectId.isValid(UserId)){
-    return res.status(400).send({message:"Invalid User Id",success:false})
+async function GetUserOrdersController(req, res) {
+  const userId = req.UserId;
+  try {
+    if (!mongoose.Types.ObjectId.isValid) {
+      return res
+        .status(400)
+        .send({ message: 'In valid user id', success: false });
+    }
+    const checkUser = await UserModel.findOne({ _id: userId });
+    if (!checkUser) {
+      return res
+        .status(400)
+        .send({ message: 'Please sign up', success: false });
+    }
+
+    const orders = await OrderModel.find({
+      user: userId,
+      orderStatus: { $ne: 'Cancelled' },
+    }).populate('orderItems');
+
+    return res
+      .status(200)
+      .send({ message: 'Data Successfully fetched', success: true, orders });
+  } catch (er) {
+    return res.status(500).send({ message: er.message, success: false });
+  }
 }
-const checkUser=await UserModel.findOne({_id:UserId})
-        if (!checkUser){
-            return res.status(400).send({message:'user not present please signup',success:false})
-        }
-        const orders=await UserModel.findOne({_id:UserId})
-        return res.status(200).send({ message: 'Data Successfully fetched', success: true, orders });
-}catch(er){
-    return res.status(500).send({message:er.message,success:true})
-}
-}
-module.exports={
-    CreateOrder,
-    getUserOrders
-}
+
+module.exports = {
+  CreateOrderController,
+  GetUserOrdersController,
+};
